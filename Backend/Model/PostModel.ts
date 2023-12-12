5b223c90a741aa9837e526cd436aa5d6a8d2c9af
Ahmed Nasr <S21510777EG@std.aou.edu.eg>
2023-12-12 23:14:25 +0200

    Create Post Module

---
+++ Backend/Model/PostModel.ts
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+import joi from 'joi';
+const PostSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:2,
+        maxlength:200
+    },
+    descreption:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:2,
+        maxlength:200
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:Object,
+        default:{
+            url:"",
+            publicId:null
+        }
+    },
+    Likes:[
+        {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+        }
+        ]   
+},{timestamps:true})
+
+const Post=mongoose.model("Post",PostSchema);
+
+// Express validation
+
+export const validate_Create_Post=(obj:Object)=>{
+    const schema=joi.object({
+        title:joi.string().trim().required().min(2).max(200),
+        descreption:joi.string().trim().required().min(2).max(200),
+        category:joi.string().trim().required()
+    })
+    return schema.validate(obj)
+}
+
+export const validate_Update_post=(obj:Object)=>{
+    const schema=joi.object({
+        title:joi.string().trim().min(2).max(200),
+        description:joi.string().trim().min(2).max(200),
+        category:joi.string().trim()
+    })
+    return schema.validate(obj)
+}
+
+module.exports={
+    Post,
+    validate_Create_Post,
+    validate_Update_post
+}
